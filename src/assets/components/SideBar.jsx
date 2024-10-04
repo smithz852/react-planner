34f2f67e55e7d4ec8f52411663ba07edf6b1f462
Zach Smith <zachsmith852@gmail.com>
2024-10-03 20:45:20 -0700

    add header and btn to sidebar

--- src/assets/components/SideBar.jsx
+++ src/assets/components/SideBar.jsx
@@ -3,11 +3,14 @@ import '../components/SideBar.css'
 export default function SideBar() {
   return (
     <>
-
       <div className="sideBar">
         <h1 className="myProjectsHeader">My Projects</h1>
+        <section className='flexCenter'>
+          <button className='addProjectBtn'>
+            <p className='btnText'>+ Add Project</p>
+            </button>
+        </section>
       </div>
-      
     </>
   )
 }
