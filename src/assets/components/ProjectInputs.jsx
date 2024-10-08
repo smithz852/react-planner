39f9130d69c1e847c255abd64d98254211be76da
Zach Smith <zachsmith852@gmail.com>
2024-10-07 21:10:43 -0700

    add save input functionality

--- src/assets/components/ProjectInputs.jsx
+++ src/assets/components/ProjectInputs.jsx
@@ -1,11 +1,44 @@
-import '../components/ProjectInputs.css'
+import { useState, useRef } from "react";
+import "../components/ProjectInputs.css";
 
 export default function ProjectInputs() {
+  const [projectInfo, setProjectInfo] = useState();
+
+  const projectName = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleClick() {
+     setProjectInfo({
+      projectName: projectName.current.value,
+      description: description.current.value,
+      dueDate: dueDate.current.value
+     })
+  }
+
   return (
     <>
-      <input type="text" value='Project Name' className='center inputStyle'/>
-      <input type="text" value='Description'className='spacing inputStyle desc'/>
-      <input type="text" value='Due Date' className='inputStyle '/>
+    
+      <input
+        type="text"
+        ref={projectName}
+        placeholder="Project"
+        className="center inputStyle"
+      />
+      <input
+        type="text"
+        ref={description}
+        placeholder="Description"
+        className="spacing inputStyle desc"
+      />
+      <input
+        type="text"
+        ref={dueDate}
+        placeholder="Due Date"
+        className="inputStyle "
+      />
+      <button onClick={handleClick}>Save</button>
+      <button>Cancel</button>
     </>
-  )
+  );
 }
